a40a4e0d8d96f1d9c22842ff83ee3c9d86ca98a3
Edward Bennigsen <edward.bennigsen@protonmail.com>
2018-02-14 22:27:36 +0000

    Final version of code from tutorial

--- sufbot_stevens.py
+++ sufbot_stevens.py
@@ -3,6 +3,7 @@
 import nltk
 from nltk.probability import ConditionalFreqDist
 from nltk.corpus import genesis
+import random
 
 fname = "titles/illinois.txt"
 
@@ -14,14 +15,31 @@ tokens = nltk.word_tokenize(" ".join(titles))
 
 text = nltk.Text(tokens)
 
-ngrams = nltk.ngrams(text, 2)
-
-def cfd(ngrams):
+def cfd(text, maxhistory):
+    ngrams = []
+    for i in range(2, maxhistory + 1):
+        ngrams += nltk.ngrams(text, i)
     return ConditionalFreqDist([(tuple(a), b) for *a,b in ngrams])
 
-def generate(seed, cfd, maxcount=10):
+
+def generate(seed, cfd, maxhistory, maxcount=10):
     for i in range(maxcount):
-        seed.append(cfd[tuple(seed[-1:])].max())
+        for j in range(maxhistory-1, 0, -1):
+            if tuple(seed[-j:]) in cfd:
+                valuesum=sum(cfd[tuple(seed[-j:])].values())
+                value=random.randint(0, valuesum)
+                for key in cfd[tuple(seed[-j:])].keys():
+                    value-=cfd[tuple(seed[-j:])][key]
+                    if value <= 0:
+                        seed.append(key)
+                        break
+                break
+            else:
+                continue
     return seed
 
-print(generate(['Concerning'], cfd(ngrams)))
+maxhistory = 5
+cfd = cfd(text, maxhistory)
+generated = generate(['Hawk'], cfd, maxhistory)
+
+print(" ".join(generated))
